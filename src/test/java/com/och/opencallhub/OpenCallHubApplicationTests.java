f8717a5234d962d59cfe5672aef0e08e06acd231
danmo <775144922@qq.com>
2024-07-10 10:30:17 +0800

    commit

---
+++ src/test/java/com/och/opencallhub/OpenCallHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.och.opencallhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenCallHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
